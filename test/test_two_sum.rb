31cf38609d2c0545efac5cbbb21196dc53d7e68a
Lafwind <lafwind@gmail.com>
2015-04-02 21:47:00 +0800

    fix a small bug #Two_sum

--- test/test_two_sum.rb
+++ test/test_two_sum.rb
@@ -5,6 +5,6 @@ class TestTwoSum < Minitest::Test
   def test_two_sum
     assert_equal [1, 2], two_sum([2, 7, 11, 15], 9)
     assert_equal [2, 3], two_sum([2, 7, 11, 15], 18)
-    assert_equal [2, 3], two_sum([1, 2, 18, 15], 19)
+    assert_equal [1, 3], two_sum([1, 2, 18, 15], 19)
   end
 end
