5fd0c047fb90a218c8ddfa2a3de92b9001973f99
Lafwind <lafwind@gmail.com>
2015-04-08 22:52:35 +0800

    fix a bug #Two sum

--- lib/two_sum.rb
+++ lib/two_sum.rb
@@ -1,12 +1,8 @@
 def two_sum(numbers, target)
   numbers.each_index do |i|
     t = target-numbers[i]
-    if numbers.include?(t)
-      if i > t
-        return [numbers.index(t)+1, i+1]
-      else
-        return [i+1, numbers.index(t)+1]
-      end
+    if numbers.include?(t) && i != numbers.index(t)
+      return [i+1, numbers.index(t)+1].sort
     end
   end
 end
