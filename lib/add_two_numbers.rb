9799c7e6aa013940a3e035fb1203de2a69d58159
Lafwind <lafwind@gmail.com>
2015-04-09 21:05:40 +0800

    solve #Add_two_numbers

---
+++ lib/add_two_numbers.rb
@@ -0,0 +1,65 @@
+# You are given two linked lists representing two non-negative numbers.
+# The digits are stored in reverse order and each of their nodes contain
+# a single digit. Add the two numbers and return it as a linked list.
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+
+class ListNode
+  attr_accessor :val, :next
+
+  def initialize(val)
+    @val = val
+    @next = nil
+  end
+end
+
+def add_two_numbers(l1, l2)
+  if l1 != nil || l2 != nil
+    return l1 if l2 == nil
+    return l2 if l1 == nil
+    tmp = l1.val + l2.val > 9 ? 1 : 0
+    v = (l1.val + l2.val) % 10
+    root = ans = ListNode.new(v)
+    l1, l2 = l1.next, l2.next
+  end
+
+  while l1 != nil && l2 != nil
+    v = (l1.val + l2.val + tmp) % 10
+    tmp = l1.val + l2.val + tmp > 9 ? 1 : 0
+    ans.next = ListNode.new(v)
+    ans = ans.next
+    l1, l2 = l1.next, l2.next
+  end
+
+  ans.next = l1 != nil ? l1 : l2
+
+  if tmp == 1
+    if ans.next == nil
+      ans.next = ListNode.new(1)
+    else
+      ans = ans.next
+      while ans.next != nil
+        if ans.val == 9
+          ans.val = 0
+          tmp = 1
+        else
+          ans.val += tmp
+          tmp = 0
+          break
+        end
+        ans = ans.next
+      end
+
+      if tmp == 1
+        if ans.val == 9
+          ans.val = 0
+          ans.next = ListNode.new(1)
+        else
+          ans.val += 1
+        end
+      end
+    end
+  end
+
+  return root
+end
---
+++ test/test_add_two_numbers.rb
@@ -0,0 +1,35 @@
+require 'minitest/autorun'
+require './lib/add_two_numbers'
+
+class TestAddTwoNumbers < Minitest::Test
+
+  # def setup
+  #   @l10 = ListNode.new(2)
+  #   @l11 = ListNode.new(4)
+  #   @l12 = ListNode.new(3)
+  #   # @l13 = ListNode.new(3)
+  #   @l10.next = @l11
+  #   @l11.next = @l12
+  #   # @l12.next = @l13
+
+  #   @l20 = ListNode.new(5)
+  #   @l21 = ListNode.new(6)
+  #   @l22 = ListNode.new(4)
+  #   # @l23 = ListNode.new(6)
+  #   @l20.next = @l21
+  #   @l21.next = @l22
+  #   # @l22.next = @l23
+
+  #   @l30 = ListNode.new(7)
+  #   @l31 = ListNode.new(0)
+  #   @l32 = ListNode.new(8)
+  #   # @l33 = ListNode.new(9)
+  #   @l30.next = @l31
+  #   @l31.next = @l32
+  #   # @l32.next = @l33
+  # end
+
+  # def test_add_two_numbers
+  #   assert_equal @l30, add_two_numbers(@l10, @l20)
+  # end
+end
