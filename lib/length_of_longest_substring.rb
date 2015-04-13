ae7a988c54e00f07e370dc418c2cf15ef99ad4e4
Lafwind <lafwind@gmail.com>
2015-04-13 22:25:52 +0800

    solve #Length_of_longest_substring

--- lib/length_of_longest_substring.rb
+++ lib/length_of_longest_substring.rb
@@ -3,4 +3,20 @@
 # which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
 
 def length_of_longest_substring(s)
+  longest = 0
+  map = {}
+  first, i = 0, 0
+
+  while i < s.length
+    if map.has_key? s[i]
+      first = first > map[s[i]] + 1 ? first : map[s[i]] + 1
+    end
+
+    map[s[i]] = i
+    longest = longest > (i - first + 1) ? longest : (i - first + 1)
+
+    i += 1
+  end
+
+  longest
 end
