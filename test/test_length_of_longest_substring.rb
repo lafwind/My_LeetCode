0e40814cff952db8427d5b462384c7063dc4f346
Lafwind <lafwind@gmail.com>
2015-04-13 22:58:51 +0800

    add more testcases for #Length_of_longest_substring and the solution still works

--- test/test_length_of_longest_substring.rb
+++ test/test_length_of_longest_substring.rb
@@ -6,5 +6,9 @@ class TestLengthOfLongestSubstring < Minitest::Test
   def test_length_of_longest_substring
     assert_equal 3, length_of_longest_substring("abcabcbb")
     assert_equal 1, length_of_longest_substring("aaaaaa")
+    assert_equal 0, length_of_longest_substring("")
+    assert_equal 2, length_of_longest_substring("al")
+    assert_equal 8, length_of_longest_substring("waglfdelaxyz")
+    assert_equal 2, length_of_longest_substring("aaooo")
   end
 end
